c18e7f10f67e3b27243dc36e25bb671f864ea4eb
Mikita <Kuznikit@gmail.com>
2021-10-16 23:17:14 +0300

    QaseIO

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,8 +5,8 @@ import org.testng.annotations.BeforeMethod;
 import utils.PropertyReader;
 
 public class BaseTest {
-    String user;
-    String password;
+    public String user;
+    public String password;
     @BeforeMethod
     public void setup() {
         //ImplicitlyWait
@@ -14,9 +14,12 @@ public class BaseTest {
         //Browser Type
         Configuration.browser = "chrome";
         //Base URL
-        Configuration.baseUrl = PropertyReader.getProperty("qase.url");
-        user = PropertyReader.getProperty("qase.user");
-        password = PropertyReader.getProperty("qase.pass");
+        Configuration.baseUrl = System.getenv().getOrDefault("QASE_URL",
+                PropertyReader.getProperty("qase.url"));
+        user = System.getenv().getOrDefault("QASE_USER",
+                PropertyReader.getProperty("qase.user"));
+        password = System.getenv().getOrDefault("QASE_PASSWORD",
+                PropertyReader.getProperty("qase.pass"));
         Configuration.startMaximized = true;
         Configuration.headless = false;
         //All clicks
