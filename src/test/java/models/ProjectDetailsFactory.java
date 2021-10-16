31d1d538261cc30ca2c59f380953aef7205a095d
Mikita <Kuznikit@gmail.com>
2021-10-16 20:12:30 +0300

    QaseIO

--- src/test/java/models/ProjectDetailsFactory.java
+++ src/test/java/models/ProjectDetailsFactory.java
@@ -1,4 +1,19 @@
 package models;
 
 public class ProjectDetailsFactory {
+    public static ProjectDetails getData(){
+        return new ProjectDetails(
+                "Title description",
+                "Deprecated",
+                "Test cases without suite",
+                "Blocker",
+                "High",
+                "Functional",
+                "API",
+                "Yes",
+                "Not set",
+                "Positive",
+                "Automated");
+    }
+
 }
--- src/test/java/pages/ProjectDetailsPage.java
+++ src/test/java/pages/ProjectDetailsPage.java
@@ -1,4 +1,25 @@
 package pages;
 
+import models.ProjectDetails;
+import wrappers.DropDown;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class ProjectDetailsPage {
+    public static final String TITLE = "#title";
+
+    public ProjectDetailsPage selectingDropDownFields(ProjectDetails projectDetails) {
+        $(TITLE).sendKeys(projectDetails.getTitle());
+        new DropDown("Status").select(projectDetails.getStatus());
+        new DropDown("Suite").select(projectDetails.getSuite());
+        new DropDown("Severity").select(projectDetails.getSeverity());
+        new DropDown("Priority").select(projectDetails.getPriority());
+        new DropDown("Type").select(projectDetails.getType());
+        new DropDown("Layer").select(projectDetails.getLayer());
+        new DropDown("Is Flaky").select(projectDetails.getIsFlaky());
+        new DropDown("Milestone").select(projectDetails.getMilestone());
+        new DropDown("Behavior").select(projectDetails.getBehavior());
+        new DropDown("Automation status").select(projectDetails.getAutomationStatus());
+        return this;
+    }
 }
